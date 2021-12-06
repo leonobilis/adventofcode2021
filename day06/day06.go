8c6e25c7bbc47739856a624041d89ad7531e5db5
marlew <lev7@vp.pl>
2021-12-06 06:47:59 +0100

    day 6

---
+++ day06/day06.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	utils "github.com/lev7/adventofcode2021"
+)
+
+const maxTimer = 9
+
+func parseInput(input string) (output []int) {
+	for _, s := range strings.Split(input, ",") {
+		output = append(output, utils.Atoi(s))
+	}
+	return
+}
+
+func simulation(lanternfishes []int, days int) int {
+	lCount := make([]int, maxTimer)
+	for _, l := range lanternfishes {
+		lCount[l]++
+	}
+	for i := 0; i < days; i++ {
+		newLanternfishes := lCount[0]
+		for j := 1; j < maxTimer; j++ {
+			lCount[j-1] = lCount[j]
+		}
+		lCount[8] = newLanternfishes
+		lCount[6] += newLanternfishes
+	}
+	sum := 0
+	for _, l := range lCount {
+		sum += l
+	}
+	return sum
+}
+
+func p1(lanternfishes []int) int {
+	return simulation(lanternfishes, 80)
+}
+
+func p2(lanternfishes []int) int {
+	return simulation(lanternfishes, 256)
+}
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	entries := parseInput(string(input))
+	fmt.Printf("Part 1: %v\n", p1(entries))
+	fmt.Printf("Part 2: %v\n", p2(entries))
+}
