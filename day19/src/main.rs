1b4018e6d2e9f77fbdd840236fe369e90239ab51
marlew <lev7@vp.pl>
2022-01-02 16:06:35 +0100

    day19 fmt + clippy

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,25 +1,26 @@
-use std::fs;
-use std::ops::{Add, Sub};
-use std::cmp::{PartialEq, PartialOrd, Eq, Ord};
-use std::hash::Hash;
 use itertools::Itertools;
-
+use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
+use std::fs;
+use std::hash::Hash;
+use std::ops::{Add, Sub};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 struct Point {
-    x: i32, y: i32, z: i32
+    x: i32,
+    y: i32,
+    z: i32,
 }
 
 impl Point {
     fn new() -> Self {
-        Point{x: 0, y: 0, z: 0}
+        Point { x: 0, y: 0, z: 0 }
     }
 
-    fn from_iter<'a, I: Iterator::<Item=&'a str>>(mut iter: I) -> Self {
-        Point{
+    fn from_iter<'a, I: Iterator<Item = &'a str>>(mut iter: I) -> Self {
+        Point {
             x: iter.next().unwrap().parse().unwrap(),
             y: iter.next().unwrap().parse().unwrap(),
-            z: iter.next().unwrap().parse().unwrap()
+            z: iter.next().unwrap().parse().unwrap(),
         }
     }
 
@@ -35,7 +36,7 @@ impl Add for Point {
         Self {
             x: self.x + other.x,
             y: self.y + other.y,
-            z: self.z + other.z
+            z: self.z + other.z,
         }
     }
 }
@@ -47,109 +48,227 @@ impl Sub for Point {
         Self {
             x: self.x - other.x,
             y: self.y - other.y,
-            z: self.z - other.z
+            z: self.z - other.z,
         }
     }
 }
 
 struct Scanner {
     pos: Point,
-    points: Vec<Point>
+    points: Vec<Point>,
 }
 
 impl Scanner {
-    fn rotations(& self) -> Vec<Scanner> {
+    fn rotation<F>(&self, f: F) -> Scanner
+    where
+        F: FnMut(&Point) -> Point,
+    {
+        Scanner {
+            pos: self.pos,
+            points: self.points.iter().map(f).collect(),
+        }
+    }
+
+    fn rotations(&self) -> Vec<Scanner> {
         vec![
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.x, y:  p.y, z:  p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.x, y: -p.z, z:  p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.x, y: -p.y, z: -p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.x, y:  p.z, z: -p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.x, y: -p.y, z:  p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.x, y: -p.z, z: -p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.x, y:  p.y, z: -p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.x, y:  p.z, z:  p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.z, y:  p.x, z: -p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.y, y:  p.x, z: -p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.z, y:  p.x, z:  p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.y, y:  p.x, z:  p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.z, y: -p.x, z: -p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.y, y: -p.x, z:  p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.z, y: -p.x, z:  p.y}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.y, y: -p.x, z: -p.z}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.y, y: -p.z, z:  p.x}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.z, y: -p.y, z:  p.x}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.y, y:  p.z, z:  p.x}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.z, y:  p.y, z:  p.x}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.z, y:  p.y, z: -p.x}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.y, y:  p.z, z: -p.x}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x: -p.z, y: -p.y, z: -p.x}).collect()},
-            Scanner{pos: self.pos, points: self.points.iter().map(|p| Point{x:  p.y, y: -p.z, z: -p.x}).collect()}
+            self.rotation(|p| Point {
+                x: p.x,
+                y: p.y,
+                z: p.z,
+            }),
+            self.rotation(|p| Point {
+                x: p.x,
+                y: -p.z,
+                z: p.y,
+            }),
+            self.rotation(|p| Point {
+                x: p.x,
+                y: -p.y,
+                z: -p.z,
+            }),
+            self.rotation(|p| Point {
+                x: p.x,
+                y: p.z,
+                z: -p.y,
+            }),
+            self.rotation(|p| Point {
+                x: -p.x,
+                y: -p.y,
+                z: p.z,
+            }),
+            self.rotation(|p| Point {
+                x: -p.x,
+                y: -p.z,
+                z: -p.y,
+            }),
+            self.rotation(|p| Point {
+                x: -p.x,
+                y: p.y,
+                z: -p.z,
+            }),
+            self.rotation(|p| Point {
+                x: -p.x,
+                y: p.z,
+                z: p.y,
+            }),
+            self.rotation(|p| Point {
+                x: -p.z,
+                y: p.x,
+                z: -p.y,
+            }),
+            self.rotation(|p| Point {
+                x: p.y,
+                y: p.x,
+                z: -p.z,
+            }),
+            self.rotation(|p| Point {
+                x: p.z,
+                y: p.x,
+                z: p.y,
+            }),
+            self.rotation(|p| Point {
+                x: -p.y,
+                y: p.x,
+                z: p.z,
+            }),
+            self.rotation(|p| Point {
+                x: p.z,
+                y: -p.x,
+                z: -p.y,
+            }),
+            self.rotation(|p| Point {
+                x: p.y,
+                y: -p.x,
+                z: p.z,
+            }),
+            self.rotation(|p| Point {
+                x: -p.z,
+                y: -p.x,
+                z: p.y,
+            }),
+            self.rotation(|p| Point {
+                x: -p.y,
+                y: -p.x,
+                z: -p.z,
+            }),
+            self.rotation(|p| Point {
+                x: -p.y,
+                y: -p.z,
+                z: p.x,
+            }),
+            self.rotation(|p| Point {
+                x: p.z,
+                y: -p.y,
+                z: p.x,
+            }),
+            self.rotation(|p| Point {
+                x: p.y,
+                y: p.z,
+                z: p.x,
+            }),
+            self.rotation(|p| Point {
+                x: -p.z,
+                y: p.y,
+                z: p.x,
+            }),
+            self.rotation(|p| Point {
+                x: p.z,
+                y: p.y,
+                z: -p.x,
+            }),
+            self.rotation(|p| Point {
+                x: -p.y,
+                y: p.z,
+                z: -p.x,
+            }),
+            self.rotation(|p| Point {
+                x: -p.z,
+                y: -p.y,
+                z: -p.x,
+            }),
+            self.rotation(|p| Point {
+                x: p.y,
+                y: -p.z,
+                z: -p.x,
+            }),
         ]
     }
 
     fn normalize(&self, reference: &Scanner) -> Option<Scanner> {
-        let rotations = self.rotations();
-        let matched  = rotations.iter()
-            .map(|s| (s, s.points.iter()
-                .flat_map(|p| reference.points.iter()
-                        .map(move |rp| *rp - *p)
-                    )
-                    .sorted()
-                    .group_by(|x| *x)
-                    .into_iter()
-                    .map(|(k, v)| (k, v.count()))
-                    .max_by_key(|(_, c)| *c)
-                    .unwrap()
+        self.rotations()
+            .iter()
+            .map(|s| {
+                (
+                    s,
+                    s.points
+                        .iter()
+                        .flat_map(|p| reference.points.iter().map(move |rp| *rp - *p))
+                        .sorted()
+                        .group_by(|x| *x)
+                        .into_iter()
+                        .map(|(k, v)| (k, v.count()))
+                        .max_by_key(|(_, c)| *c)
+                        .unwrap(),
                 )
-            ).skip_while(|(_, (_, c))| *c < 12).next();
-        
-        match matched {
-            Some((scanner, (diff, _))) => Some(Scanner{
+            })
+            .find(|(_, (_, c))| *c >= 12)
+            .map(|(scanner, (diff, _))| Scanner {
                 pos: self.pos + diff,
-                points: scanner.points.iter().map(|p| *p + diff).collect()
-            }),
-            None => None
-        }
+                points: scanner.points.iter().map(|p| *p + diff).collect(),
+            })
     }
 }
 
-fn parse_input(inp: &String) -> Vec<Scanner> {
+fn parse_input(inp: &str) -> Vec<Scanner> {
     inp.split("\n\n")
         .map(|scanner| Scanner {
             pos: Point::new(),
-            points: scanner.split("\n").skip(1).map(|point| Point::from_iter(point.split(","))).collect()
+            points: scanner
+                .lines()
+                .skip(1)
+                .map(|point| Point::from_iter(point.split(',')))
+                .collect(),
         })
-    .collect()
+        .collect()
 }
 
 fn get_normalized_scanners(mut scanners: Vec<Scanner>) -> Vec<Scanner> {
-    let mut ref_scanners = Vec::with_capacity(scanners.len()); 
+    let mut ref_scanners = Vec::with_capacity(scanners.len());
     let mut result_scanners = Vec::with_capacity(scanners.len());
 
     ref_scanners.push(scanners.pop().unwrap());
 
     while !scanners.is_empty() {
         let ref_scanner = ref_scanners.pop().unwrap();
-        let new_references: Vec<_> = scanners.iter()
+        let new_references: Vec<_> = scanners
+            .iter()
             .map(|sc| sc.normalize(&ref_scanner))
             .collect();
 
         let mut keep = new_references.iter().map(|sc| sc.is_none());
         scanners.retain(|_| keep.next().unwrap());
-        
-        ref_scanners.extend(new_references.into_iter().filter_map(|nr| nr));
+
+        ref_scanners.extend(new_references.into_iter().flatten());
         result_scanners.push(ref_scanner);
     }
     result_scanners.extend(ref_scanners);
     result_scanners
 }
 
-fn p1(scanners: &Vec<Scanner>) -> usize {
-    scanners.iter().flat_map(|sc| sc.points.iter()).unique().count()
+fn p1(scanners: &[Scanner]) -> usize {
+    scanners
+        .iter()
+        .flat_map(|sc| sc.points.iter())
+        .unique()
+        .count()
 }
 
-fn p2(scanners: &Vec<Scanner>) -> i32 {
-    scanners.iter().flat_map(|s1| scanners.iter().map(move |s2| s1.pos.manhatan(&s2.pos))).max().unwrap()
+fn p2(scanners: &[Scanner]) -> i32 {
+    scanners
+        .iter()
+        .flat_map(|s1| scanners.iter().map(move |s2| s1.pos.manhatan(&s2.pos)))
+        .max()
+        .unwrap()
 }
 
 fn main() {
